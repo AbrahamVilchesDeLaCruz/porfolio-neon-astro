05b5a67c59805d07dddba4f3c649b7f515a23d4a
AbrahamVilchesDeLaCruz <abrahamvilchesdelacruz@gmail.com>
2025-07-23 20:31:06 +0200

    style: improve UI responsive for experience section

--- src/components/experience/ExperienceTimelineItem.tsx
+++ src/components/experience/ExperienceTimelineItem.tsx
@@ -11,21 +11,24 @@ export const ExperienceTimelineItem = ({ experience, index }: ExperienceTimeline
   return (
     <div className="relative w-full mb-16">
       {/* Mobile: bloque único */}
-      <div className="flex flex-col md:hidden items-center w-full">
-        <div className="bg-mariner-950 border-2 border-mariner-400 rounded-lg p-8 shadow-lg w-full max-w-md flex flex-col items-center">
-          <img
-            src={experience.image}
-            alt={`${experience.company} logo`}
-            loading="lazy"
-            className="w-32 h-32 object-contain mb-6"
-          />
-          <h3 className="text-2xl font-semibold text-mariner-100 mb-1 text-center">
-            {experience.role}
-          </h3>
-          <p className="text-lg text-mariner-300 mb-1 text-center">{experience.company}</p>
-          <span className="text-sm text-mariner-400 mb-4 block text-center">
-            {experience.period}
-          </span>
+      <div className="flex flex-col md:hidden items-center ">
+        <div className="bg-mariner-950 border-2 border-mariner-400 rounded-lg p-4 shadow-lg w-full max-w-xs flex flex-col items-center">
+          <header className="flex items-center justify-around justify-items-cente gap-4">
+            <div className="w-2/3">
+              <h3 className="text-xl font-semibold text-mariner-100">{experience.role}</h3>
+              <p className="text-md text-mariner-300">{experience.company}</p>
+              <span className="text-sm text-mariner-400">{experience.period}</span>
+            </div>
+            <div className="w-1/2 flex items-center justify-center">
+              <img
+                src={experience.image}
+                alt={`${experience.company} logo`}
+                loading="lazy"
+                className="max-w-full max-h-full"
+              />
+            </div>
+          </header>
+          <hr className="w-4/5 border-2 border-mariner-400 shadow-neon my-3" />
           <div className="mb-2 w-full">
             <h4 className="text-lg font-bold text-mariner-100 neon-effect mb-1 text-center">
               Backend
